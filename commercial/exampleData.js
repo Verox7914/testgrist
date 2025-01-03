1d9e50304c8775f48fde051efab667f008d8e23e
Verox7914 <99517492+Verox7914@users.noreply.github.com>
2025-01-03 01:30:33 +0100

    Add files via upload

--- commercial/exampleData.js
+++ commercial/exampleData.js
@@ -2,11 +2,9 @@ const exampleData = {
   Number: 14999,
   Issued: Date.parse('2024-10-12') / 1000,
   Due: Date.parse('2024-11-12') / 1000,
-
   Invoicer: {
     Name: 'Thunderous Applause',
     Street1: '812 Automated Rd',
-    Street2: null,
     City: 'New York',
     State: 'NY',
     Zip: '10003',
@@ -14,7 +12,6 @@ const exampleData = {
     Phone: '+1-800-111-1111',
     Website: 'applause.com',
   },
-
   Client: {
     Name: 'Monkeys Juggling',
     Street1: '100 Banana St',
@@ -22,35 +19,11 @@ const exampleData = {
     State: 'NJ',
     Zip: '07048',
   },
-
   Items: [
-    {
-      Description: 'Rivelatore ottico di fumo analogico indirizzato con isolatore integrato, sensibilità programmabile. 240 indirizzi. Certificazione CPR EN54pt7/pt17 LPCB.',
-      Price: 89,
-      Quantity: 3,
-      Total: 253.65,
-      Code: 'ED100',
-      Discount: '5%',
-    },
-    {
-      Description: 'Base standard per rivelatori convenzionali (IRIS) ed indirizzati (ENEA)',
-      Price: 8,
-      Quantity: 6,
-      Total: 44.8,
-      Code: 'EB0010',
-      Discount: '5%',
-    },
-    {
-      Description: 'Rivelatore Multisensor ottico di fumo e di temperatura analogico indirizzato con isolatore integrato, sensibilit à e modalità di funzionamento programmabili. 240 indirizzi. Certificazione CPR EN54pt5/pt17 LPCB.',
-      Price: 93,
-      Quantity: 3,
-      Total: 265.05,
-      Code: 'ED300',
-      Discount: '5%',
-    },
+    { Description: 'Optical smoke detector', Price: 89, Quantity: 3, Total: 253.65, Code: 'ED100' },
+    { Description: 'Standard detector base', Price: 8, Quantity: 6, Total: 44.8, Code: 'EB0010' },
   ],
-
-  Subtotal: 563.5,
-  Taxes: 123.97,
-  Total: 687.47,
+  Subtotal: 298.45,
+  Taxes: 65.46,
+  Total: 363.91,
 };
