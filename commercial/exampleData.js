065924950e448c05588b31064b8b7d4f061c7afc
Verox7914 <99517492+Verox7914@users.noreply.github.com>
2025-01-03 22:35:14 +0100

    Add files via upload

--- commercial/exampleData.js
+++ commercial/exampleData.js
@@ -1,10 +1,12 @@
 const exampleData = {
   Number: 14999,
-  Issued: Date.parse('2024-10-12') / 1000,
-  Due: Date.parse('2024-11-12') / 1000,
+  Issued: Date.parse('2020-10-12') / 1000,
+  Due: Date.parse('2020-11-12') / 1000,
+
   Invoicer: {
     Name: 'Thunderous Applause',
     Street1: '812 Automated Rd',
+    Street2: null,
     City: 'New York',
     State: 'NY',
     Zip: '10003',
@@ -12,6 +14,7 @@ const exampleData = {
     Phone: '+1-800-111-1111',
     Website: 'applause.com',
   },
+
   Client: {
     Name: 'Monkeys Juggling',
     Street1: '100 Banana St',
@@ -19,11 +22,24 @@ const exampleData = {
     State: 'NJ',
     Zip: '07048',
   },
+
   Items: [
-    { Description: 'Optical smoke detector', Price: 89, Quantity: 3, Total: 253.65, Code: 'ED100' },
-    { Description: 'Standard detector base', Price: 8, Quantity: 6, Total: 44.8, Code: 'EB0010' },
+    {
+      Description: 'Wolf Whistle',
+      Price: 35,
+      Quantity: 3,
+      Total: 105,
+    },
+    {
+      Description: 'Bravo',
+      Price: 30,
+      Quantity: 17,
+      Total: 510,
+    },
   ],
-  Subtotal: 298.45,
-  Taxes: 65.46,
-  Total: 363.91,
+
+  Subtotal: 615,
+  Deduction: null,
+  Taxes: null,
+  Total: 615,
 };
