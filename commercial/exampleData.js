be63232ceca7f25cb4dc1424bcf6b6858e8fd95e
Verox7914 <99517492+Verox7914@users.noreply.github.com>
2025-01-06 14:09:35 +0100

    Update exampleData.js

--- commercial/exampleData.js
+++ commercial/exampleData.js
@@ -2,7 +2,7 @@ const exampleData = {
   Number: 14999,
   Issued: Date.parse('2024-10-12') / 1000,
   Due: Date.parse('2024-11-12') / 1000,
-  Reference: job xxxx,
+  Reference: 'job xxxx',
   },
   
   Invoicer: {
